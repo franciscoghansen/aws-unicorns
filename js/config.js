453a1d8e0fcd91a2aabc9367333801f7258974ff
Francisco Hansen <francisco.hansen@levadata.com>
2022-06-09 18:24:27 -0300

    New invokeUrl

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://se4xkpcznb.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://fpsdgn9y3k.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
